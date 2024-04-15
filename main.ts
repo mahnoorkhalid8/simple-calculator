545daf89600dc4c6bc4e711e0493bfff2bbb6730
mahnoorkhalid8 <mahnoorkhalid814@gmail.com>
2024-04-16 01:26:20 +0500

    tweak the text and added console in the end

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@
 // shabang
 
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{ message: "Enter first number" , type: "number" , name: "firstNumber"},
-{ message: "Enter second number" , type: "number" , name: "secondNumber"},
+const answer = await inquirer.prompt([{ message: "Enter your first number" , type: "number" , name: "firstNumber"},
+{ message: "Enter your second number" , type: "number" , name: "secondNumber"},
 { message: "Select one of the operators to perform operations" , type: "list" , name: "operators" ,
 choices: ["Addition" , "Subtraction", "Multiplication", "Division"], }])
 
@@ -23,3 +23,4 @@ else if (answer.operators === "Division"){
 else{
     console.log("Please select valid operator")
 }
+console.log("The End");
